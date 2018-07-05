348ae4e01c00b9a977426ae807e2de0b6ed4be1f
HUHEDONG\Administrator <1182738042@qq.com>
2018-07-05 15:47:16 +0800

    新增中奖信息表

---
+++ src/main/java/com/dl/task/dao/DlPrintLotteryThirdRewardMapper.java
@@ -0,0 +1,7 @@
+package com.dl.task.dao;
+
+import com.dl.base.mapper.Mapper;
+import com.dl.task.model.DlPrintLotteryThirdReward;
+
+public interface DlPrintLotteryThirdRewardMapper extends Mapper<DlPrintLotteryThirdReward> {
+}
