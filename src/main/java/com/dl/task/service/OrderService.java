f9ae7c56a4208dd5a8329839aa7b33c6430f6584
HUHEDONG\Administrator <1182738042@qq.com>
2018-07-10 11:23:47 +0800

    增加流水记录参数并更改流水

--- src/main/java/com/dl/task/service/OrderService.java
+++ src/main/java/com/dl/task/service/OrderService.java
@@ -1253,7 +1253,8 @@ public class OrderService extends AbstractService<Order> {
 				UserAccount userAccount = new UserAccount();
 				userAccount.setUserId(userId);
 				String accountSn = SNGenerator.nextSN(SNBusinessCodeEnum.ACCOUNT_SN.getCode());
-				userAccount.setAmount(payLog.getOrderAmount());
+				userAccount.setAmount(BigDecimal.ZERO.subtract(payLog.getOrderAmount()));
+	            userAccount.setOrderSn(orderSn);
 				userAccount.setAccountSn(accountSn);
 				userAccount.setBonusPrice(BigDecimal.ZERO);
 				userAccount.setProcessType(3);
@@ -1274,7 +1275,6 @@ public class OrderService extends AbstractService<Order> {
 				userAccount.setPaymentName(payName);
 				userAccount.setThirdPartName(payName);
 				userAccount.setThirdPartPaid(payLog.getOrderAmount());
-				userAccount.setUserId(payLog.getUserId());
 				int rst = userAccountMapper.insertUserAccountBySelective(userAccount);
 				log.info("生成回滚账户流水返回值" + rst);				
 //		进行预出票
