2565c77cc659d80a64ebbddeae711498322a70ac
HUHEDONG\Administrator <1182738042@qq.com>
2018-08-06 10:22:41 +0800

    增加出票适配器，

---
+++ src/main/java/com/dl/task/printlottery/PrintLotteryAdapter.java
@@ -0,0 +1,35 @@
+package com.dl.task.printlottery;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PrintLotteryAdapter {
+
+	/**
+	 * 投注
+	 */
+	public void toStake(){
+		
+	}
+	/**
+	 * 查询投注信息
+	 */
+	public void queryStake(){
+		
+	}
+	/**
+	 * 主动兑奖 有些公司需要
+	 */
+	public void award(){
+		
+	}
+	/**
+	 * 查询第三方奖金
+	 */
+	public void queryAward(){
+		
+	}
+	private void queryAccountBalance(){
+		
+	}
+}
