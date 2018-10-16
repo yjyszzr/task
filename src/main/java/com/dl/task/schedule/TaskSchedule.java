7d59f747541ff3f223753471dd1c5114bf652de6
wenhuaiteng <wenhuaiteng@126.com>
2018-10-16 18:20:52 +0800

    远程...

--- src/main/java/com/dl/task/schedule/TaskSchedule.java
+++ src/main/java/com/dl/task/schedule/TaskSchedule.java
@@ -474,7 +474,7 @@ public class TaskSchedule {
 		log.info("^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^");
 		log.info("^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^");
 		log.info("^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^");
-//		EmptyParam emptyParam = new EmptyParam();
-//		iArtifiPrintLotteryService.artifiTaskTimer(emptyParam);
+		EmptyParam emptyParam = new EmptyParam();
+		iArtifiPrintLotteryService.artifiTaskTimer(emptyParam);
 	}
 }
