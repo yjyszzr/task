672dad82d47aadb87ec85ced9203aa264703a530
lifeilong <850479604@qq.com>
2019-04-13 10:25:03 +0800

    支付修改

--- src/main/java/com/dl/task/schedule/TaskSchedule.java
+++ src/main/java/com/dl/task/schedule/TaskSchedule.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import com.dl.base.param.EmptyParam;
 import com.dl.lottery.api.IArtifiPrintLotteryService;
 import com.dl.shop.payment.api.IpaymentService;
 import com.dl.task.configurer.URLConfig;
@@ -241,12 +242,12 @@ public class TaskSchedule {
 	/**
 	 * 第三方支付的query 订单
 	 */
-//	@Scheduled(cron = "${task.schedule.order.pay.timeout}")
-//	public void timerOrderQueryScheduled() {
-//		log.info("第三方支付定时任务开始");
-//		EmptyParam emptyParam = new EmptyParam();
-//		ipaymentService.timerOrderQueryScheduled(emptyParam);
-//	}
+	@Scheduled(cron = "${task.schedule.order.pay.timeout}")
+	public void timerOrderQueryScheduled() {
+		log.info("第三方支付定时任务开始");
+		EmptyParam emptyParam = new EmptyParam();
+		ipaymentService.timerOrderQueryScheduled(emptyParam);
+	}
 
 	/**
 	 * 订单支付成功逻辑处理
@@ -285,12 +286,12 @@ public class TaskSchedule {
 	/**
 	 * 第三方支付的query 充值
 	 */
-//	@Scheduled(cron = "${task.schedule.recharge.pay.timeout}")
-//	public void timerRechargeQueryScheduled() {
-//		log.info("第三方支付定时任务开始");
-//		EmptyParam emptyParam = new EmptyParam();
-//		ipaymentService.timerRechargeQueryScheduled(emptyParam);
-//	}
+	@Scheduled(cron = "${task.schedule.recharge.pay.timeout}")
+	public void timerRechargeQueryScheduled() {
+		log.info("第三方支付定时任务开始");
+		EmptyParam emptyParam = new EmptyParam();
+		ipaymentService.timerRechargeQueryScheduled(emptyParam);
+	}
 
 
 
