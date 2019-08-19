a6f9cc997115375c1347e00132716563364bb4a4
lifeilong <lifeilong@caixiaomi.com>
2019-08-19 18:02:55 +0800

    xg

---
+++ src/main/java/com/dl/task/dao/ActivityUserInfoMapper.java
@@ -0,0 +1,11 @@
+package com.dl.task.dao;
+
+import com.dl.base.mapper.Mapper;
+import com.dl.task.model.ActivityUserInfo;
+
+public interface ActivityUserInfoMapper extends Mapper<ActivityUserInfo> {
+	
+	Integer updateActivityUserInfo();
+
+
+}
